63e3ad579bffabebcf8abf6fa5f1f806eadef910
qhf <651583725@qq.com>
2021-03-16 10:19:18 +0800

    最新打包

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     autoprefixer: {},
     "postcss-px-to-viewport": {
       viewportWidth: 375,  // 视窗的宽度，对应的是我们设计稿的宽度
-      viewportHeight: 700,  // 视窗的高度，对应的是我们设计稿的高度 (也可以不配置)
+      viewportHeight: 667,  // 视窗的高度，对应的是我们设计稿的高度 (也可以不配置)
       unitPrecision: 5,  // 指定 'px' 转化为视窗单位值的小数位数(很多时候无法整除)
       viewportUnit: 'vw',  //指定需要转化成的视图单位，建议使用vw
       selectorBlackList: ['ignore','tab-bar','tab-bar-item'],  //指定不需要转化的类
